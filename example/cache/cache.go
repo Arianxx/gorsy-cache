72223146a4b880fd596334fe1558f4f6294dfcf8
Arianxx <34032486+Arianxx@users.noreply.github.com>
2019-03-29 12:04:45 +0800

    chore: add example

--- example/cache/cache.go
+++ example/cache/cache.go
@@ -1,1 +1,34 @@
-package cache
+package main
+
+import (
+	"fmt"
+	"github.com/arianxx/gorsy-cache"
+)
+
+func loader(key interface{}) (interface{}, error) {
+	return "baka!", nil
+}
+
+func beforeEvict(key, value interface{}) {
+	fmt.Printf("%v: %v was be removed\n", key, value)
+}
+
+func main() {
+	builder, err := gorsy_cache.NewBuilder(gorsy_cache.SIMPLE, 10)
+	if err != nil {
+		panic("build cache: " + err.Error())
+	}
+	cache := builder.
+		SetName("rocket").
+		SetDefaultExpiration(gorsy_cache.DefaultExpiration).
+		SetLoaderFunc(loader).
+		SetBeforeEvictedFunc(beforeEvict).
+		Build()
+	cache.Set(1, 2)
+	cache.Set(2, 3)
+	fmt.Println(cache.Get(1))
+	fmt.Println(cache.Get(3))
+	fmt.Println(cache.GetOnlyPresent(3))
+	cache.Remove(2)
+	fmt.Println(cache.Get(2))
+}
