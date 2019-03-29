ea740aab963ec33d9087550458ef93a7ff49b690
Arianxx <34032486+Arianxx@users.noreply.github.com>
2019-03-29 12:04:15 +0800

    feat: add KeyNotFound error

--- error.go
+++ error.go
@@ -1,1 +1,17 @@
 package gorsy_cache
+
+import "fmt"
+
+type KeyNotFoundError struct {
+	Cache Cache
+	Key   interface{}
+	Err   error
+}
+
+func (e *KeyNotFoundError) Error() string {
+	s := "`%s`: key `%s` not found in the cache store"
+	if e.Err != nil {
+		s += " with loader function error: " + e.Err.Error()
+	}
+	return fmt.Sprintf(s, e.Cache.getBaseCache().Name, e.Key)
+}
