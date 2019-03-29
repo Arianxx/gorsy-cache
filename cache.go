0622dc02104beb4f4691d4953c9572c3863431d0
Arianxx <34032486+Arianxx@users.noreply.github.com>
2019-03-29 12:03:45 +0800

    feat: eliminate the reflection

--- cache.go
+++ cache.go
@@ -7,16 +7,26 @@ import (
 	"time"
 )
 
+const (
+	NoExpiration      = -1
+	DefaultExpiration = 0
+)
+
 // cacheCollected collects the specific cache object constructor.
 var cacheCollected = make(map[string]func(int) interface{})
 
+// cacheCounter distinguish anonymous cache store
+var cacheCounter int
+
 // Cache represents a interface used by the user to r/w the cache store.
 type Cache interface {
-	init()
+	getBaseCache() *baseCache
 
+	Init()
 	Get(key interface{}) (interface{}, error)
-	Set(key, value interface{}) error
-	SetWithExpire(key, value interface{}, duration time.Duration) error
+	GetOnlyPresent(key interface{}) (interface{}, bool)
+	Set(key, value interface{})
+	SetWithExpire(key, value interface{}, duration time.Duration)
 	Has(key interface{}) bool
 	Remove(key interface{}) bool
 	Keys() []interface{}
@@ -29,85 +39,90 @@ type Cache interface {
 type baseCache struct {
 	sync.RWMutex
 	size int
+	Name string
 	// expiration provides a global expiration information for a cache store.
 	Expiration time.Duration
 	LoaderFunc
-	SerializerFunc
-	DeserializerFunc
 	BeforeEvictedFunc
 }
 
-const (
-	NoExpiration      = -1
-	DefaultExpiration = 0
-)
-
 type (
 	LoaderFunc func(key interface{}) (interface{}, error)
-	SerializerFunc func(key, value interface{}) (interface{}, error)
-	DeserializerFunc func(key, value interface{}) (interface{}, error)
 	BeforeEvictedFunc func(key, value interface{})
 )
 
 // cacheBuilder used to build a specific cache.
 type cacheBuilder struct {
-	cache interface{}
+	cache Cache
+	bc    *baseCache
 }
 
-func New(name string, size int) (*cacheBuilder, error) {
+// NewBuilder receive a constant cache name and a cache size, return a specific cache builder.
+// A error will be returned if the specific cache is not registered or the cache implementation is invalid.
+func NewBuilder(name string, size int) (*cacheBuilder, error) {
 	f, ok := cacheCollected[name]
 	if !ok {
 		return nil, fmt.Errorf("no specific cache was found")
 	}
-	builder := &cacheBuilder{f(size)}
-	if err := builder.checkCacheValid(); err != nil {
+
+	c := f(size)
+	if err := checkCacheValid(c); err != nil {
 		return nil, fmt.Errorf("specific cache invalid: %s", err.Error())
 	}
 
+	builder := &cacheBuilder{cache: c.(Cache)}
+	builder.bc = builder.cache.getBaseCache()
 	return builder, nil
 }
 
+// Build a cache store by the previous setups.
+// Typically it will perform some tasks such as allocating cache space.
 func (c *cacheBuilder) Build() Cache {
-	cache := c.cache.(Cache)
-	cache.init()
-	return cache
+	if c.bc.Name == "" {
+		c.bc.Name = fmt.Sprintf("cache: %d", cacheCounter)
+		cacheCounter++
+	}
+	c.cache.Init()
+	return c.cache
+}
+
+func (c *cacheBuilder) SetName(n string) *cacheBuilder {
+	c.bc.Name = n
+	return c
 }
 
 func (c *cacheBuilder) SetDefaultExpiration(t time.Duration) *cacheBuilder {
-	p := c.getFieldAddrInterface("Expiration").(*time.Duration)
-	*p = t
+	c.bc.Expiration = t
 	return c
 }
 
 func (c *cacheBuilder) SetLoaderFunc(f LoaderFunc) *cacheBuilder {
-	p := c.getFieldAddrInterface("LoaderFunc").(*LoaderFunc)
-	*p = f
+	c.bc.LoaderFunc = f
 	return c
 }
 
-func (c *cacheBuilder) getFieldAddrInterface(name string) interface{} {
-	v := reflect.ValueOf(c.cache).Elem()
-	f := v.FieldByName(name)
-	return f.Addr().Interface()
+func (c *cacheBuilder) SetBeforeEvictedFunc(f BeforeEvictedFunc) *cacheBuilder {
+	c.bc.BeforeEvictedFunc = f
+	return c
 }
 
-func (c *cacheBuilder) checkCacheValid() error {
-	if !c.implementedCache() {
+func checkCacheValid(c interface{}) error {
+	if !implementedCache(c) {
 		return fmt.Errorf("cache has not implement the Cache interface")
 	}
-	if !c.inheritedBaseCache() {
+	if !inheritedBaseCache(c) {
 		return fmt.Errorf("cache has not inherited baseCache")
 	}
 	return nil
 }
 
-func (c *cacheBuilder) implementedCache() bool {
-	_, ok := c.cache.(Cache)
+func implementedCache(c interface{}) bool {
+	_, ok := c.(Cache)
 	return ok
 }
 
-func (c *cacheBuilder) inheritedBaseCache() bool {
-	field, ok := reflect.ValueOf(c.cache).Elem().Type().FieldByName("baseCache")
+func inheritedBaseCache(c interface{}) bool {
+	field, ok := reflect.ValueOf(c).Elem().Type().FieldByName("baseCache")
 	if !ok || fmt.Sprint(field.Type.Name()) != "baseCache" {
 		return false
 	}
---
+++ error.go
@@ -0,0 +1,1 @@
+package gorsy_cache
