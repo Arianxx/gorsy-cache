ee9517511da8e87405c90f5933e3ea158c427c2a
Arianxx <34032486+Arianxx@users.noreply.github.com>
2019-03-29 12:04:29 +0800

    feat: add simple cache store

--- simple.go
+++ simple.go
@@ -1,1 +1,214 @@
 package gorsy_cache
+
+import (
+	"time"
+)
+
+func init() {
+	cacheCollected[SIMPLE] = func(size int) interface{} {
+		return &simpleCache{baseCache{size: size}, nil}
+	}
+}
+
+const (
+	SIMPLE = "simpleCache"
+)
+
+type simpleCache struct {
+	baseCache
+	items map[interface{}]*simpleItem
+}
+
+func (c *simpleCache) Init() {
+	c.items = make(map[interface{}]*simpleItem, c.size)
+	if c.Expiration == DefaultExpiration {
+		c.Expiration = 60
+	}
+}
+
+func (c *simpleCache) getBaseCache() *baseCache {
+	return &c.baseCache
+}
+
+func (c *simpleCache) Get(key interface{}) (interface{}, error) {
+	c.RLock()
+	defer c.RUnlock()
+
+	return c.get(key, true)
+}
+
+func (c *simpleCache) GetOnlyPresent(key interface{}) (interface{}, bool) {
+	c.RLock()
+	defer c.RUnlock()
+
+	v, exist := c.get(key, false)
+	if exist != nil {
+		return nil, false
+	} else {
+		return v, true
+	}
+}
+
+func (c *simpleCache) get(key interface{}, fromLoader bool) (interface{}, error) {
+	item, ok := c.items[key]
+	if ok && !item.isExpired() {
+		return item.value, nil
+	}
+
+	if fromLoader {
+		return c.getFromLoader(key)
+	} else {
+		return nil, &KeyNotFoundError{}
+	}
+}
+
+func (c *simpleCache) getFromLoader(key interface{}) (interface{}, error) {
+	var v interface{}
+	var err error
+	if c.LoaderFunc != nil {
+		v, err = c.LoaderFunc(key)
+		if err != nil {
+			return nil, &KeyNotFoundError{c, key, err}
+		}
+	}
+
+	return v, &KeyNotFoundError{c, key, nil}
+}
+
+func (c *simpleCache) Set(key, value interface{}) {
+	c.Lock()
+	defer c.Unlock()
+
+	c.set(key, value, DefaultExpiration)
+}
+
+func (c *simpleCache) set(key, value interface{}, expiration time.Duration) {
+	if len(c.items) == c.size {
+		c.evict(1)
+	}
+
+	item := &simpleItem{key, value, nil}
+	item.setExpiration(expiration, c)
+	c.items[key] = item
+}
+
+func (c *simpleCache) SetWithExpire(key, value interface{}, expiration time.Duration) {
+	c.Lock()
+	defer c.Unlock()
+
+	c.set(key, value, expiration)
+}
+
+func (c *simpleCache) evict(num int) {
+	keys := make([]interface{}, 0)
+	for k, v := range c.items {
+		if v.isExpired() {
+			keys = append(keys, k)
+		}
+
+		if len(keys) >= num {
+			break
+		}
+	}
+
+	for _, k := range keys {
+		c.remove(k)
+	}
+}
+
+func (c *simpleCache) Has(key interface{}) bool {
+	c.RLock()
+	defer c.RUnlock()
+
+	item, ok := c.items[key]
+	return ok && !item.isExpired()
+}
+
+func (c *simpleCache) Remove(key interface{}) bool {
+	c.Lock()
+	defer c.Unlock()
+
+	return c.remove(key)
+}
+
+func (c *simpleCache) remove(key interface{}) bool {
+	item, ok := c.items[key]
+	if !ok {
+		return false
+	}
+	delete(c.items, key)
+
+	if c.BeforeEvictedFunc != nil {
+		c.BeforeEvictedFunc(key, item.value)
+	}
+
+	return !item.isExpired()
+}
+
+func (c *simpleCache) Keys() []interface{} {
+	c.RLock()
+	defer c.RUnlock()
+
+	keys := make([]interface{}, 0)
+	for k, v := range c.items {
+		if !v.isExpired() {
+			keys = append(keys, k)
+		}
+	}
+
+	return keys
+}
+
+func (c *simpleCache) CleanExpired() int {
+	c.Lock()
+	defer c.Unlock()
+
+	expiredKeys := make([]interface{}, 0)
+	for k, v := range c.items {
+		if v.isExpired() {
+			expiredKeys = append(expiredKeys, k)
+		}
+	}
+
+	for _, k := range expiredKeys {
+		c.remove(k)
+	}
+
+	return len(expiredKeys)
+}
+
+func (c *simpleCache) Flush() {
+	c.Lock()
+	defer c.Unlock()
+
+	c.Init()
+}
+
+func (c *simpleCache) Len() int {
+	return len(c.items)
+}
+
+type simpleItem struct {
+	key, value interface{}
+	expiration *time.Time
+}
+
+func (s *simpleItem) isExpired() bool {
+	if s.expiration == nil {
+		return false
+	}
+
+	return s.expiration.Before(time.Now())
+}
+
+func (s *simpleItem) setExpiration(expiration time.Duration, c *simpleCache) {
+	if expiration == DefaultExpiration {
+		expiration = c.Expiration
+	}
+	if expiration != NoExpiration {
+		t := time.Now().Add(expiration * time.Second)
+		s.expiration = &t
+	} else {
+		s.expiration = nil
+	}
+}
